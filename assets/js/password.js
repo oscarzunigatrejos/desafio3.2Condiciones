390782a9d37ae2c713348a106c51b48e61584449
oscarzunigatrejos <oscar.zuniga.trejos@gmail.com>
2024-06-21 23:12:40 -0400

    first commit

---
+++ assets/js/password.js
@@ -0,0 +1,25 @@
+const password1 = 123;
+const password2 = 789;
+
+function comprobar(){
+    let pass1 = document.getElementById("pass1").value;
+    let pass2 = document.getElementById("pass2").value;
+    let pass3 = document.getElementById("pass3").value;
+    let password_ingresada = pass1 + pass2 + pass3;
+    if(password_ingresada == password1){
+
+        document.querySelector("span").innerHTML = "Password correcto";
+        document.querySelector("span").classList.remove("text-red-500"); //remuevo color rojo
+        document.querySelector("span").classList.add("text-green-500"); // añado color verde
+    }else  if(password_ingresada == password2){
+
+        document.querySelector("span").innerHTML = "Password correcto";
+        document.querySelector("span").classList.remove("text-red-500"); //remuevo color rojo
+        document.querySelector("span").classList.add("text-green-500"); // añado color verde
+    }else{
+
+        document.querySelector("span").innerHTML = "Password incorrecto";
+        document.querySelector("span").classList.remove("text-green-500"); //remuevo color verde
+        document.querySelector("span").classList.add("text-red-500"); // añado color rojo para indicar error
+    }
+}
